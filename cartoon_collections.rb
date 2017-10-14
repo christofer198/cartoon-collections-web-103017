e575cc475633ae7c39fe8d6078bf915714624436
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-14 04:33:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index{ |val, index| puts "#{index}. #{val}"}
+  array.each_with_index{ |val, index| puts "#{index+1}. #{val}"}
 
 end
 
