ef4663a86f1ca81cf181f97e61c0a9846336236c
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-14 04:37:19 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,16 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |call|
+    if call.length > 4
+      return true
+      break
+    else
+      return false
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
